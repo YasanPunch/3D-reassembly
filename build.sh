1fdc8b16e841fe8a37d8ef79e73277667f2d0b35
Pundima-Lakshan <pundimal@gmail.com>
2025-04-18 08:52:09 +0530

    CPP export compile options on build for autocompletions

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Define the build directory
-BUILD_DIR="build"
-
 # Define installation directory of Open3D
 OPEN3D_ROOT="${HOME}/dev/reassembly/open3d_install/cpp"
+
+# From here on out change if you really want
+
+# Define the build directory
+BUILD_DIR="build"
 
 # Create the build directory if it doesn't exist
 if [ ! -d "$BUILD_DIR" ]; then
@@ -17,7 +19,7 @@ cd "$BUILD_DIR" || exit
 
 # Run CMake to configure the project (passing Open3D_ROOT if necessary)
 echo "Running CMake configuration..."
-cmake -DOpen3D_ROOT="$OPEN3D_ROOT" ../cpp
+cmake -DOpen3D_ROOT="$OPEN3D_ROOT" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ../cpp
 
 # Build the project using make with 12 parallel jobs
 echo "Building the project using make..."
